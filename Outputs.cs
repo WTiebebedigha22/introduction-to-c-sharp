d69c538463f41250aeb7def5bb7996b726c8c46c
Ware Tiebebedigha <106900112+WTiebebedigha22@users.noreply.github.com>
2025-07-06 22:02:00 +0100

    Create Outputs.cs

---
+++ Outputs.cs
@@ -0,0 +1,21 @@
+//To output values or print text in C#, you can use the WriteLine() method:
+Console.WriteLine("Hello World!");
+Console.WriteLine("I am Learning C#");
+Console.WriteLine("It is Awesome!");
+/*
+Hello World!
+I am Learning c#
+It is awesome!
+*/
+//You can also output numbers, and perform mathematical calculations:
+Console.WriteLine(3 + 3);
+//The output: 6 
+/*
+The Write Method
+
+There is also a Write() method, which is similar to WriteLine().
+The only difference is that it does not insert a new line at the end of the output:
+*/
+Console.Write("Hello World! ");
+Console.write("I will Print on the same line.");
+//The output: "Hello World! I will print on the same line.
