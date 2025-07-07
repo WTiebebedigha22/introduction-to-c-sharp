dbe7e4d58d1a602d9383f314b861010c1aef995b
Ware Tiebebedigha <106900112+WTiebebedigha22@users.noreply.github.com>
2025-07-07 10:45:25 +0100

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -1,1 +1,8 @@
+/*
+Constants
+If you don't want others (or yourself) to overwrite existing values, you can add the const keyword in front of the variable type.
 
+This will declare the variable as "constant", which means unchangeable and read-only:
+*/
+const int myNum = 15;
+myNum = 20; // error
